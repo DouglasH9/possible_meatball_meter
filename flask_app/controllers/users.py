d04ce77a93a1ff6d466298c97c0328031ac25818
Doug Herman <douglash9882@gmail.com>
2022-01-05 21:50:13 -0600

    registration logic done

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -8,3 +8,27 @@ bcrypt = Bcrypt(app)
 @app.route("/")
 def load_home_page():
     return render_template("login_reg.html")
+
+@app.route("/register", methods=["POST"])
+def register_user():
+
+    # if info doesn't pass validations, redirect to login page
+    if not User.validate_email_reg(request.form):
+        return redirect("/")
+
+    # hash entered password if password passes validations
+    pw_hash = bcrypt.generate_password_hash(request.form["pass_ent"])
+
+    # create user instance for database with form info
+    data = {
+        "fName" : request.form["fName"],
+        "lName" : request.form["lName"],
+        "email" : request.form["email"],
+        "password" : pw_hash
+    }
+
+    id = User.register_user(data)
+    # put user id in session after successful registration
+    session["user_id"] = id
+    # redirect to success page after successful registration
+    return redirect("/reg_success")
