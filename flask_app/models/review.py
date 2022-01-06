fc48ab365f937f15d442be6ee839dd737a6ebfa4
Doug Herman <douglash9882@gmail.com>
2022-01-06 15:31:05 -0600

    review validations

--- flask_app/models/review.py
+++ flask_app/models/review.py
@@ -15,10 +15,26 @@ class Review:
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
 
+        # define self.user for Review as empty object to be filled when review's user info is pulled from database
         self.user = {}
 
     @classmethod
     def add_review(cls, data):
-        query = "INSERT INTO reviews (review, is_blasted, rating, user_id, restaurant_name, is_affordable, created_at, updated_at) VALUES (%(review)s, %(is_blasted)s, %(rating)s, %(user_id)s, %(restaurant_name)s, %(is_affordable)s, %(created_at)s, %(updated_at)s)"
+        # insert query to add reviews to databased
+        query = "INSERT INTO reviews (review, is_blasted, rating, user_id, restaurant_name, is_affordable, created_at, updated_at) VALUES (%(review)s, %(is_blasted)s, %(rating)s, %(user_id)s, %(restaurant_name)s, %(is_affordable)s, NOW(), NOW())"
         print (query)
         return connectToMySQL("meatball_meter").query_db(query, data)
+
+    @staticmethod
+    def validate_review(data):
+        is_valid = True
+        if (len(data["review"]) < 10):
+            flash("Review must be at least 10 characters, you absolute fool!")
+            is_valid = False
+        if (float(data["rating"]) < 0):
+            flash("Rating must be greater than or equal to zero, genius!")
+            is_valid = False
+        if (len(data["restaurant_name"]) < 2):
+            flash("Restaurant name must be more than 2 characters, dingus!")
+            is_valid = False
+        
