7d55faf23ff1c96d988cec80701f99783b6aeb75
Doug Herman <douglash9882@gmail.com>
2022-01-06 23:56:29 -0600

    added push to db logic for reviews

--- flask_app/controllers/reviews.py
+++ flask_app/controllers/reviews.py
@@ -7,3 +7,21 @@ from flask_app.models import user
 @app.route("/add_review")
 def render_add_a_review():
     return render_template("/add_review.html")
+
+@app.route("push_review", methods=["POST"])
+def push_review_to_db():
+    if "user_id" not in session:
+        return redirect("/logout")
+
+    data = {
+        "restaurant_name" : request.form["restaurant_name"],
+        "review" : request.form["review"],
+        "rating" : request.form["rating"], 
+        "is_affordable" : request.form["is_affordable"],
+        "user_id" : session["user_id"]
+    }
+    if not Review.validate_review(data):
+        return redirect("/dashboard")
+
+    Review.add_review(data)
+    return redirect("/dashboard")
