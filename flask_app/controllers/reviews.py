6c2a3e3796bd36ab1fcc49a34b598ce9955bdc38
Doug Herman <douglash9882@gmail.com>
2022-01-07 01:29:36 -0600

    fixed review form and boolean issues

--- flask_app/controllers/reviews.py
+++ flask_app/controllers/reviews.py
@@ -8,20 +8,25 @@ from flask_app.models import user
 def render_add_a_review():
     return render_template("/add_review.html")
 
-@app.route("push_review", methods=["POST"])
+@app.route("/push_review", methods=["POST"])
 def push_review_to_db():
+    # get rid of people who's user id not in session
     if "user_id" not in session:
         return redirect("/logout")
 
+    # set Review object data
     data = {
         "restaurant_name" : request.form["restaurant_name"],
         "review" : request.form["review"],
+        "is_blasted" : request.form["is_blasted"],
         "rating" : request.form["rating"], 
         "is_affordable" : request.form["is_affordable"],
         "user_id" : session["user_id"]
     }
+    # redirect with flash messages if data doesn't pass validations
     if not Review.validate_review(data):
         return redirect("/dashboard")
 
+    # add review if passed validations
     Review.add_review(data)
     return redirect("/dashboard")
