7c81e68d6fa0fc1783a17f80a94a35f0453062ac
Doug Herman <douglash9882@gmail.com>
2022-01-12 17:34:31 -0600

    checkbox js working

--- flask_app/static/js/checkField.js
+++ flask_app/static/js/checkField.js
@@ -3,14 +3,20 @@ let affordBox = document.getElementById("is_affordable")
 let checkField1 = document.getElementById("check_field1")
 let checkField2 = document.getElementById("check_field2")
 
+console.log(blastBox)
+
 blastBox.addEventListener("change", function() {
-    if (this.checked) {
+    if (this.checked == true) {
         checkField1.id = "checked_field"
+    } else {
+        checkField1.id = "check_field1"
     }
 })
 
 affordBox.addEventListener("change", function() {
-    if (this.checked) {
+    if (this.checked == true) {
         checkField2.id = "checked_field"
+    } else {
+        checkField2.id = "check_field2"
     }
 })
