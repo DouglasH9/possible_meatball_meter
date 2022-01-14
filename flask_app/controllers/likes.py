c05e574f7e6f764956aa877a9fb0d6652edfddae
Doug Herman <douglash9882@gmail.com>
2022-01-14 16:19:31 -0600

    more logic

--- flask_app/controllers/likes.py
+++ flask_app/controllers/likes.py
@@ -3,11 +3,15 @@ from flask_app import app
 from flask import redirect, session
 from flask_app.models.user import User
 from flask_app.models.review import Review
+from flask_app.models.like import Like
 
-@app.route("/add_like/<int:reviewID>")
+@app.route("/add_like/<int:reviewID>", methods=["POST"])
 def add_like(reviewID):
 
     data = {
         "user_id" : session["user_id"],
         "review_id" : reviewID,
     }
+    
+    if Like.check_for_like_in_db(data):
+        Like.add_like(data)
