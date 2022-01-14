22c13bb442df2c29debb8e2a469d03172100e7df
Doug Herman <douglash9882@gmail.com>
2022-01-14 15:47:01 -0600

    building likes model and controller

---
+++ flask_app/controllers/likes.py
@@ -0,0 +1,6 @@
+from types import MethodDescriptorType
+from flask_app import app
+from flask import redirect, request
+from flask_app.models.user import User
+from flask_app.models.review import Review
+
